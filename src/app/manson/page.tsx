7ac2b444327445905d03837df8c84c1d90db5fde
MatheusVBLima <matheus.lima@ccc.ufcg.edu.br>
2024-08-12 22:05:53 -0300

    add: ciro embeed twitch

--- src/app/manson/page.tsx
+++ src/app/manson/page.tsx
@@ -2,14 +2,27 @@ import React from "react";
 
 export default function page() {
   return (
-    <div className="flex h-[calc(100vh-7.75rem)] items-center justify-center">
-      <iframe
-        src="https://player.twitch.tv/?channel=ovelhera&parent=rastaflix.vercel.app"
-        allowFullScreen
-        scrolling="no"
-        height="378"
-        width="620"
-      ></iframe>
+    <div className="flex h-[calc(100vh-7.75rem)] items-center justify-center gap-6">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <p className="text-3xl font-semibold">Ovelha</p>
+        <iframe
+          src="https://player.twitch.tv/?channel=ovelhera&parent=rastaflix.vercel.app"
+          allowFullScreen
+          scrolling="no"
+          height="378"
+          width="620"
+        ></iframe>
+      </div>
+      <div className="flex flex-col items-center justify-center gap-4">
+        <p className="text-3xl font-semibold">Ciro</p>
+        <iframe
+          src="https://player.twitch.tv/?channel=ciromtv&parent=rastaflix.vercel.app"
+          allowFullScreen
+          scrolling="no"
+          height="378"
+          width="620"
+        ></iframe>
+      </div>
     </div>
   );
 }
