5b65a888792ed8420bd85525dd5c6a9fdb249977
MatheusVBLima <matheus.lima@ccc.ufcg.edu.br>
2024-08-13 11:18:51 -0300

    add: chat

--- src/app/manson/page.tsx
+++ src/app/manson/page.tsx
@@ -1,28 +1,68 @@
-import React from "react";
+"use client";
 
-export default function page() {
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
+export default function Manson() {
+  const [isOvelha, setIsOvelha] = useState(true);
+  const [isCiro, setIsCiro] = useState(false);
+
+  function handleIsOvelha() {
+    setIsOvelha(true);
+    setIsCiro(false);
+  }
+
+  function handleIsCiro() {
+    setIsOvelha(false);
+    setIsCiro(true);
+  }
   return (
-    <div className="container flex h-[calc(100vh-7.75rem)] items-center justify-center gap-6">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <p className="text-3xl font-semibold">Ovelha</p>
-        <iframe
-          src="https://player.twitch.tv/?channel=ovelhera&parent=rastaflix.vercel.app"
-          allowFullScreen
-          scrolling="no"
-          height="378"
-          width="620"
-        ></iframe>
+    <div className="container flex h-[calc(100vh-7.75rem)] flex-col items-center gap-20">
+      <div className="mt-8 flex w-full flex-col items-center justify-between gap-4 lg:flex-row">
+        <p className="text-center text-lg font-semibold">
+          Sala da revolta: assista sem dar dinheiro para esses vagabundos
+        </p>
+        <nav className="flex items-center gap-4">
+          <Button onClick={handleIsOvelha} variant={"outline"}>
+            Assistir Ovelha
+          </Button>
+          <Button onClick={handleIsCiro} variant={"outline"}>
+            Assistir Ciro
+          </Button>
+        </nav>
       </div>
-      <div className="flex flex-col items-center justify-center gap-4">
-        <p className="text-3xl font-semibold">Ciro</p>
-        <iframe
-          src="https://player.twitch.tv/?channel=ciromtv&parent=rastaflix.vercel.app"
-          allowFullScreen
-          scrolling="no"
-          height="378"
-          width="620"
-        ></iframe>
-      </div>
+      {isOvelha && (
+        <div className="flex flex-col items-center justify-center gap-4">
+          <p className="text-3xl font-semibold">Ovelha</p>
+          <iframe
+            src="https://player.twitch.tv/?channel=ovelhera&parent=rastaflix.vercel.app"
+            allowFullScreen
+            scrolling="no"
+            className="h-[250px] w-[350px] lg:h-[400px] lg:w-[600px]"
+          ></iframe>
+          <iframe
+            id="chat_embed"
+            src="https://www.twitch.tv/embed/ovelhera/chat?parent=rastaflix.vercel.app"
+            className="h-[250px] w-[350px] lg:h-[400px] lg:w-[600px]"
+          ></iframe>
+        </div>
+      )}
+      {isCiro && (
+        <div className="flex flex-col items-center justify-center gap-4">
+          <p className="text-3xl font-semibold">Ciro</p>
+          <iframe
+            src="https://player.twitch.tv/?channel=ciromtv&parent=rastaflix.vercel.app"
+            allowFullScreen
+            scrolling="no"
+            className="h-[250px] w-[350px] lg:h-[400px] lg:w-[600px]"
+          ></iframe>
+          <iframe
+            id="chat_embed"
+            src="https://www.twitch.tv/embed/ciromtv/chat?parent=rastaflix.vercel.app"
+            className="h-[250px] w-[350px] lg:h-[400px] lg:w-[600px]"
+          ></iframe>
+        </div>
+      )}
     </div>
   );
 }
