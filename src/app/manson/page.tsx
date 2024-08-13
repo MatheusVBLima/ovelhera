71190c8596c93c4eeb058657ee81aaad454900a4
MatheusVBLima <matheus.lima@ccc.ufcg.edu.br>
2024-08-12 21:55:51 -0300

    add: twitch embeed

---
+++ src/app/manson/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="flex h-[calc(100vh-7.75rem)] items-center justify-center">
+      <iframe
+        src="https://player.twitch.tv/?channel=ovelhera&parent=https://rastaflix.vercel.app/"
+        allowFullScreen
+        scrolling="no"
+        height="378"
+        width="620"
+      ></iframe>
+    </div>
+  );
+}
