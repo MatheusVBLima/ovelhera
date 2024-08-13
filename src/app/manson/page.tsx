9db8b6582f68640f5a005ae20b32c690a2e23378
MatheusVBLima <matheus.lima@ccc.ufcg.edu.br>
2024-08-12 22:01:08 -0300

    fix: embeed video

--- src/app/manson/page.tsx
+++ src/app/manson/page.tsx
@@ -4,7 +4,7 @@ export default function page() {
   return (
     <div className="flex h-[calc(100vh-7.75rem)] items-center justify-center">
       <iframe
-        src="https://player.twitch.tv/?channel=ovelhera&parent=https://rastaflix.vercel.app/"
+        src="https://player.twitch.tv/?channel=ovelhera&parent=rastaflix.vercel.app"
         allowFullScreen
         scrolling="no"
         height="378"
