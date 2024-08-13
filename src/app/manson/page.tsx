fe1941c5fbfac0400fa644b1f4fd4b49194c47af
MatheusVBLima <matheus.lima@ccc.ufcg.edu.br>
2024-08-12 22:28:27 -0300

    fix: embeed container

--- src/app/manson/page.tsx
+++ src/app/manson/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function page() {
   return (
-    <div className="container grid h-[calc(100vh-7.75rem)] grid-cols-1 place-items-center items-center gap-6 md:grid-cols-2 xl:grid-cols-3">
+    <div className="container flex h-[calc(100vh-7.75rem)] items-center justify-center gap-6">
       <div className="flex flex-col items-center justify-center gap-4">
         <p className="text-3xl font-semibold">Ovelha</p>
         <iframe
