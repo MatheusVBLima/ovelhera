bfc836f2e6d91f57391806df5c7e4fb90f5b8ff0
MatheusVBLima <matheus.lima@ccc.ufcg.edu.br>
2024-12-21 18:03:09 -0300

    delete: timer

--- src/app/awards/page.tsx
+++ src/app/awards/page.tsx
@@ -2,35 +2,8 @@
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Separator } from '@/components/ui/separator'
-import { useEffect, useState } from 'react'
 
 export default function RastaAwardsResults() {
-  const [timeLeft, setTimeLeft] = useState(getTimeLeft())
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setTimeLeft(getTimeLeft())
-    }, 1000)
-
-    return () => clearInterval(timer)
-  }, [])
-
-  function getTimeLeft() {
-    const difference = +new Date('2024-12-22') - +new Date()
-    let timeLeft = {}
-
-    if (difference > 0) {
-      timeLeft = {
-        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-        minutes: Math.floor((difference / 1000 / 60) % 60),
-        seconds: Math.floor((difference / 1000) % 60),
-      }
-    }
-
-    return timeLeft
-  }
-
   return (
     <div className="h-[calc(100vh-7.75rem)] flex items-center justify-center">
       <Card className="w-full max-w-md bg-secondary/20 backdrop-blur-sm">
@@ -50,16 +23,6 @@ export default function RastaAwardsResults() {
           <p className="text-4xl text-center font-bold text-red-700">
             22/12/2024
           </p>
-          <div className="grid grid-cols-4 gap-2 mt-4">
-            {Object.keys(timeLeft).map(interval => (
-              <div key={interval} className="flex flex-col items-center">
-                <span className="text-3xl font-bold text-green-800">
-                  {timeLeft[interval as keyof typeof timeLeft]}
-                </span>
-                <span className="text-sm text-red-700">{interval}</span>
-              </div>
-            ))}
-          </div>
         </CardContent>
       </Card>
     </div>
